2510310670e5d015c4987ed07fb3ccb9d5c33c11
周素华 <zhoushzhou@gmail.com>
2021-01-24 18:30:07 +0800

    初始化项目

---
+++ src/test/java/company/zsh/first/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package company.zsh.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
